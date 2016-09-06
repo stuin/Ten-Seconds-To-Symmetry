3870d83c1a6effe8836e362783e3179b48a20b1a
stuin <stuin01@gmail.com>
2016-09-05 19:22:29 -0700

    Created initial files

---
+++ src/com/stuin/Ten_Seconds_To_Symmetry/Game.java
@@ -0,0 +1,12 @@
+package com.stuin.Ten_Seconds_To_Symmetry;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Game extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
+}
