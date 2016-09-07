2ba0a5fe8c402a6c52cbbc1b52f1e8de6d563162
magital15 <talimagidson@hotmail.com>
2016-09-06 17:52:42 -0700

    more tests

--- src/com/stuin/Ten_Seconds_To_Symmetry/Game.java
+++ src/com/stuin/Ten_Seconds_To_Symmetry/Game.java
@@ -2,11 +2,15 @@ package com.stuin.Ten_Seconds_To_Symmetry;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.AnalogClock;
+import android.widget.Chronometer;
 
 public class Game extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+        Chronometer chronometer = (Chronometer) findViewById(R.id.chronometer);
+        chronometer.start();
     }
 }
