1317308039614bcf950402a80129d6c097af3048
Stuart <stuin01@gmail.com>
2016-11-02 19:37:49 -0700

    added icon and fixed saving

--- gen/com/stuin/tensecondstosymmetry/R.java
+++ gen/com/stuin/tensecondstosymmetry/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.stuin.TenSecondsToSymmetry;
+package com.stuin.tensecondstosymmetry;
 
 public final class R {
     public static final class attr {
--- src/com/stuin/tensecondstosymmetry/Game.java
+++ src/com/stuin/tensecondstosymmetry/Game.java
@@ -1,6 +1,8 @@
-package com.stuin.TenSecondsToSymmetry;
+package com.stuin.tensecondstosymmetry;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,40 +12,32 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Space;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Game extends Activity {
     private int size = 5;
-    private int change;
+    private int change = -1;
     private int maxColor = 3;
     private int points = 0;
     private Random random = new Random();
     private CountDownTimer countDownTimer;
-    private boolean win = false;
+    private boolean second = false;
     private int[] grid;
     private int highScore = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        //Set up app
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
         if(getActionBar() != null)getActionBar().hide();
-        TextView textView = (TextView) findViewById(R.id.button);
 
-        try {
-            FileInputStream fileInputStream = openFileInput("highScore");
-            try {
-                Scanner scanner = new Scanner(fileInputStream);
-                highScore = scanner.nextInt();
-            } catch(Exception e) {}
-            fileInputStream.close();
-        } catch(Exception e) {}
+        //Retrieve high score
+        SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        highScore = sharedPreferences.getInt("HighScore", 0);
 
-        new CountDownTimer(500,1) {
+        //Start intro animation
+        new CountDownTimer(1000,1) {
             @Override
             public void onTick(long l) {
                 Space space = (Space) findViewById(R.id.TopSpace);
@@ -62,10 +56,21 @@ public class Game extends Activity {
     }
 
     private void clear() {
+        //Clear grids
         GridLayout gridLayout = (GridLayout) findViewById(R.id.Top);
         gridLayout.removeAllViews();
         gridLayout = (GridLayout) findViewById(R.id.Bottom);
         gridLayout.removeAllViews();
+
+        //Set spacing
+        Space space = (Space) findViewById(R.id.TopSpace);
+        space.setMinimumHeight(50);
+        space = (Space) findViewById(R.id.BottomSpace);
+        space.setMinimumHeight(50);
+
+        //Clear settings
+        countDownTimer.cancel();
+        change = -1;
     }
 
     public void generate(View v) {
@@ -81,22 +86,23 @@ public class Game extends Activity {
         textView = (TextView) findViewById(R.id.Score);
         textView.setVisibility(View.GONE);
 
-        //Set spaces
-        Space space = (Space) findViewById(R.id.TopSpace);
-        space.setMinimumHeight(1000);
-        space = (Space) findViewById(R.id.BottomSpace);
-        space.setMinimumHeight(1000);
-
         //Set up grids
         GridLayout gridLayout = (GridLayout) findViewById(R.id.Top);
         setGrid(gridLayout,true);
         gridLayout = (GridLayout) findViewById(R.id.Bottom);
         setGrid(gridLayout,false);
 
+        //Show timer
         findViewById(R.id.Left).setVisibility(View.VISIBLE);
         findViewById(R.id.Right).setVisibility(View.VISIBLE);
         findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
         findViewById(R.id.HighScore).setVisibility(View.GONE);
+
+        //Set spacing
+        Space space = (Space) findViewById(R.id.TopSpace);
+        space.setMinimumHeight(1000);
+        space = (Space) findViewById(R.id.BottomSpace);
+        space.setMinimumHeight(1000);
 
         //Ready startup animation
         new CountDownTimer(500,1) {
@@ -115,12 +121,6 @@ public class Game extends Activity {
                 space.setMinimumHeight(20);
             }
         }.start();
-
-        //Set difference color
-        textView = (TextView) findViewById(change);
-        int c = random.nextInt(maxColor - 1);
-        if(grid[change] == c) c = maxColor - 1;
-        setColor(textView,c);
 
         //Start timer
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -148,83 +148,84 @@ public class Game extends Activity {
 
     private void setGrid(GridLayout gridLayout, boolean top) {
         int i = 0;
+        gridLayout.setColumnCount(size);
         for(int cell : grid) {
+            //Set default square
             TextView textView = new TextView(this);
             textView.setWidth(100);
             textView.setHeight(100);
-            setColor(textView,cell);
             gridLayout.addView(textView);
-            gridLayout.setColumnCount(size);
-            
+
+            //Set changed square
             if(i == change) {
                 textView.setOnClickListener(winListener);
-                if(top) textView.setId(change);
+                if(top) {
+                    cell = random.nextInt(maxColor - 1);
+                    if(grid[change] == cell) cell = maxColor - 1;
+                }
             } else textView.setOnClickListener(loseListener);
             i++;
-        }
-    }
 
-    private void setColor(TextView textView, int i) {
-        switch(i) {
-            case 0:
-                textView.setBackgroundColor(Color.RED);
-                break;
-            case 1:
-                textView.setBackgroundColor(Color.BLUE);
-                break;
-            case 2:
-                textView.setBackgroundColor(Color.GREEN);
-                break;
-            case 3:
-                textView.setBackgroundColor(Color.WHITE);
-                break;
-            case 4:
-                textView.setBackgroundColor(Color.BLACK);
-                break;
+            //Set color
+            switch(cell) {
+                case 0:
+                    textView.setBackgroundColor(Color.RED);
+                    break;
+                case 1:
+                    textView.setBackgroundColor(Color.BLUE);
+                    break;
+                case 2:
+                    textView.setBackgroundColor(Color.GREEN);
+                    break;
+                case 3:
+                    textView.setBackgroundColor(Color.WHITE);
+                    break;
+                case 4:
+                    textView.setBackgroundColor(Color.BLACK);
+                    break;
+            }
         }
     }
 
     private View.OnClickListener loseListener = new View.OnClickListener() {
         @Override
         public void onClick(View p1) {
+            clear();
+
+            //Set button
             TextView textView = (TextView) findViewById(R.id.button);
             String t = "Restart";
             textView.setText(t);
             textView.setVisibility(View.VISIBLE);
 
-            Space space = (Space) findViewById(R.id.TopSpace);
-            space.setMinimumHeight(50);
-            space = (Space) findViewById(R.id.BottomSpace);
-            space.setMinimumHeight(50);
-
-            countDownTimer.cancel();
-            clear();
-
+            //Show score
             textView = (TextView) findViewById(R.id.Score);
             t = "Game Lost: " + points;
             textView.setText(t);
             textView.setVisibility(View.VISIBLE);
 
+            //Check high score
             textView = (TextView) findViewById(R.id.HighScore);
             if(points > highScore) {
                 t = "New High Score";
                 highScore = points;
 
-                try {
-                    FileOutputStream fileOutputStream = openFileOutput("highScore", MODE_PRIVATE);
-                    fileOutputStream.write(points);
-                    fileOutputStream.close();
-                } catch(Exception e) {t = "File not written";}
+                SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("HighScore",highScore);
+                editor.commit();
             } else t = "High Score: " + highScore;
             textView.setText(t);
             textView.setVisibility(View.VISIBLE);
 
+            //Hide timer
             findViewById(R.id.Left).setVisibility(View.GONE);
             findViewById(R.id.Right).setVisibility(View.GONE);
             findViewById(R.id.progressBar).setVisibility(View.GONE);
-            
+
+            //Clear game
             points = 0;
-            win = false;
+            second = false;
             size = 5;
             maxColor = 3;
         }
@@ -232,38 +233,36 @@ public class Game extends Activity {
 
     @Override
     public void onBackPressed() {
-        loseListener.onClick(null);
+        if(change > -1) loseListener.onClick(null);
     }
 
     private View.OnClickListener winListener = new View.OnClickListener() {
         @Override
         public void onClick(View p1) {
+            clear();
+
+            //Set button
             TextView textView = (TextView) findViewById(R.id.button);
             String t = "Next Round";
             textView.setText(t);
             textView.setVisibility(View.VISIBLE);
 
-            Space space = (Space) findViewById(R.id.TopSpace);
-            space.setMinimumHeight(50);
-            space = (Space) findViewById(R.id.BottomSpace);
-            space.setMinimumHeight(50);
-
-            countDownTimer.cancel();
-            clear();
-            
-            if(size < 9 && win) {
+            //Check level scaling
+            if(size < 9 && second) {
                 size++;
-                win = false;
+                second = false;
             }
-            else win = true;
+            else second = true;
             if(size == 9 && maxColor < 5)  {
                 size = 5;
                 maxColor++;
             } else if(size == 9) size = 8;
 
+            //Calculate score
             ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
             points += (progressBar.getMax() - progressBar.getProgress()) * (size / 2 + maxColor);
 
+            //Show Score
             textView = (TextView) findViewById(R.id.Score);
             t = "+" + points + "+";
             textView.setText(t);
