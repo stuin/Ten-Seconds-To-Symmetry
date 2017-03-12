8fa04639aef0e079cdccb79353a05593f04e1472
Stuart <stuin01@gmail.com>
2017-03-12 15:30:41 -0700

    Using custom views

---
+++ src/com/stuin/tenseconds/Views/Grid.java
@@ -0,0 +1,15 @@
+package com.stuin.tenseconds.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridLayout;
+
+/**
+ * Created by Stuart on 3/12/2017.
+ */
+public class Grid extends GridLayout {
+    Grid(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+    }
+}
---
+++ src/com/stuin/tenseconds/Views/Timer.java
@@ -0,0 +1,15 @@
+package com.stuin.tenseconds.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Stuart on 3/12/2017.
+ */
+public class Timer extends LinearLayout {
+    Timer(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+
+    }
+}
