8c1c6f80c098b5e4a23d9ea8c100fa0292a3445d
stuin <stuin01@gmail.com>
2017-03-12 16:27:27 -0700

    Set up old branh

--- gen/com/stuin/tensecondstosymmetry/R.java
+++ gen/com/stuin/tensecondstosymmetry/R.java
@@ -8,26 +8,49 @@
 package com.stuin.tensecondstosymmetry;
 
 public final class R {
+    public static final class array {
+        public static final int game_labels=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int Bottom=0x7f050008;
-        public static final int BottomSpace =0x7f050006;
-        public static final int Left=0x7f050005;
-        public static final int Right=0x7f050003;
-        public static final int Score=0x7f050001;
-        public static final int Top=0x7f050000;
-        public static final int TopSpace =0x7f050002;
-        public static final int button=0x7f050007;
-        public static final int progressBar=0x7f050004;
+        public static final int Bottom=0x7f07000a;
+        public static final int BottomChange=0x7f07000c;
+        public static final int BottomSpace=0x7f070009;
+        public static final int Left=0x7f070008;
+        public static final int Right=0x7f070006;
+        public static final int Score=0x7f070003;
+        public static final int Top=0x7f070005;
+        public static final int TopChange=0x7f07000b;
+        public static final int TopSpace=0x7f070004;
+        public static final int button=0x7f070001;
+        public static final int linearLayout=0x7f070002;
+        public static final int progressBar=0x7f070007;
+        public static final int relative=0x7f070000;
     }
     public static final class layout {
         public static final int game=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int LeaderBoard_Expanded=0x7f040009;
+        public static final int LeaderBoard_Original=0x7f040008;
+        public static final int achievement_Black=0x7f040003;
+        public static final int achievement_Cyan=0x7f040006;
+        public static final int achievement_Magenta=0x7f040005;
+        public static final int achievement_Meet=0x7f040007;
+        public static final int achievement_RGB=0x7f040001;
+        public static final int achievement_White=0x7f040002;
+        public static final int achievement_Yellow=0x7f040004;
+        public static final int app_id=0x7f040000;
+        public static final int app_name=0x7f04000a;
+        public static final int app_start=0x7f04000b;
+    }
+    public static final class style {
+        public static final int style_base=0x7f060000;
+        public static final int style_norm=0x7f060001;
+        public static final int style_title=0x7f060002;
     }
 }
