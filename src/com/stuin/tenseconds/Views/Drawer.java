ae637938dc2abaa1bfb817cfed1f6899eeb48d82
stuin <stuin01@gmail.com>
2017-03-16 14:23:18 -0700

    started drawer

---
+++ src/com/stuin/tenseconds/Views/Drawer.java
@@ -0,0 +1,11 @@
+package com.stuin.tenseconds.Views;
+import android.widget.*;
+import android.util.*;
+import android.content.Context;
+
+public class Drawer extends LinearLayout {
+	public Drawer(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+		
+	}
+}
