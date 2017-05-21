00375bddafb557619e5724ba34e4002a9b18449b
Stuart <stuin01@gmail.com>
2017-05-20 19:27:32 -0700

    Continued work on online integration

--- Support/Purchases.java
+++ Support/Purchases.java
@@ -1,26 +1,35 @@
 package com.stuin.tenseconds.Support;
 
+import android.app.Activity;
 import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
-import aidl.com.android.vending.billing.*;
+import com.android.vending.billing.IInAppBillingService;
 
 /**
  * Created by Stuart on 5/2/2017.
  */
 public class Purchases {
 
-    IInAppBillingService mService;
+    private IInAppBillingService mService;
 
-    ServiceConnection mServiceConn = new ServiceConnection() {
+    public Purchases(Activity activity) {
+        String place = "com.android.vending";
+        Intent serviceIntent = new Intent(place + ".billing.InAppBillingService.BIND");
+        serviceIntent.setPackage(place);
+        activity.bindService(serviceIntent, mServiceConn, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mServiceConn = new ServiceConnection() {
         @Override
         public void onServiceDisconnected(ComponentName name) {
             mService = null;
         }
 
         @Override
-        public void onServiceConnected(ComponentName name,
-                                       IBinder service) {
+        public void onServiceConnected(ComponentName name, IBinder service) {
             mService = IInAppBillingService.Stub.asInterface(service);
         }
     };
